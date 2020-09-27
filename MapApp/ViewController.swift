88e84f7e05206346c44db15ad6ca12937a7c8163
KerollosNabil <kokonabi4444@gmail.com>
2020-09-27 15:40:10 +0200

    add map view and its setup

--- MapApp/ViewController.swift
+++ MapApp/ViewController.swift
@@ -6,14 +6,27 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, MKMapViewDelegate {
+    let mapView = MKMapView()
     override func viewDidLoad() {
         super.viewDidLoad()
+        mapViewSetup()
         // Do any additional setup after loading the view.
+    }
+    func mapViewSetup() {
+        view.addSubview(mapView)
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        mapView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0).isActive = true
+        mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0).isActive = true
+        mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0).isActive = true
+        mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
+        mapView.delegate = self
     }
 
 
 }
+
+
 
